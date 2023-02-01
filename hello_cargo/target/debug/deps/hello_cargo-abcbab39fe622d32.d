cd3ad285d4d09e3c86df1b7dc5ac5e3057328dfa
ziyuan wang <zw257@duke.edu>
2023-02-01 11:49:54 -0500

    use cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-abcbab39fe622d32.d
@@ -0,0 +1,5 @@
+/Users/brandonwong/IDS721/learn-rust/1 hello_world/hello_cargo/target/debug/deps/hello_cargo-abcbab39fe622d32: src/main.rs
+
+/Users/brandonwong/IDS721/learn-rust/1 hello_world/hello_cargo/target/debug/deps/hello_cargo-abcbab39fe622d32.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/brandonwong/IDS721/learn-rust/1\ hello_world/hello_cargo/target/debug/hello_cargo: /Users/brandonwong/IDS721/learn-rust/1\ hello_world/hello_cargo/src/main.rs
